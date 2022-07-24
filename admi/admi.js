f0969bb36f3fc3cbc24b2caeae6cbd40cfb77dff
Sofi Nofal <sofiamarianofal@hotmail.com>
2022-07-24 20:44:59 -0300

    sumando js

---
+++ admi/admi.js
@@ -0,0 +1,31 @@
+class producto {
+  constructor(id, nombre, categoria, precio, costo) {
+    this.id = id;
+    this.nombre = nombre;
+    this.categoria = categoria;
+    this.precio = precio;
+    this.costo = costo;
+  }
+}
+
+id = prompt("Ingrese ID");
+nombre = prompt("Ingrese nombre");
+categoria = prompt("Ingrese categoria");
+precio = parseFloat(prompt("Ingrese precio"));
+costo = parseFloat(prompt("Ingrese costo"));
+
+if(id == ''){
+    id = id + 1
+}
+
+if (precio > costo) {
+  const chipa = new producto(id, nombre, categoria, precio, costo);
+  console.log(chipa)
+} else {
+  console.log("No hay rentabilidad");
+}
+
+
+const alfa = new producto (1004, "alfajor de maicena", "pasteleria", 320, 67)
+console.log(alfa)
+//la idea es ir completando la tabla
