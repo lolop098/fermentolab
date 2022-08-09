e858fc00b3c636e84fc7760562d95e0b5b2d660c
Sofi Nofal <sofiamarianofal@hotmail.com>
2022-08-09 20:30:12 -0300

    DOM

--- admi/admi.js
+++ admi/admi.js
@@ -1,46 +1,3 @@
-/*class producto {
-  constructor(id, nombre, categoria, precio, costo) {
-    this.id = id;
-    this.nombre = nombre;
-    this.categoria = categoria;
-    this.precio = precio;
-    this.costo = costo;
-  }
-}
-
-id = prompt("Ingrese ID");
-nombre = prompt("Ingrese nombre");
-categoria = prompt("Ingrese categoria");
-precio = parseFloat(prompt("Ingrese precio"));
-costo = parseFloat(prompt("Ingrese costo"));
-
-if(id == ''){
-    id = id + 1
-}
-
-validarRentabilidad = (precio, costo) => {
-  if (precio > costo) {
-    const chipa = new producto(id, nombre, categoria, precio, costo);
-    console.log(chipa)
-  } else {
-    console.log("No hay rentabilidad");
-  }
-}
-validarRentabilidad(precio, costo)
-
-const Productos = ["medialuna", "Pan de campo"]
-const Precio = [120, 20, 54]
-
-
-Productos.push(chipa)
-console.log(Productos)
-
-const numero = document.getElementById("ID") 
-ID.innerText = id
-*/
-
-
-
 //la idea es ir completando la tabla con la info que se brinda cuando la gente aprete el "Agregar producto"
 
 class product {
@@ -51,35 +8,58 @@ class product {
     this.precio = precio;
     this.costo = costo;
   }
-   validarRentabilidad = () => {
+  validarRentabilidad = () => {
     if (precio > costo) {
-      let rentabilidad = 100*((1-(costo/precio)))
-      console.log(`La rentabilidad es de ${rentabilidad.toFixed(2)}%`)
+      let rentabilidad = 100 * (1 - costo / precio);
+      console.log(`La rentabilidad es de ${rentabilidad.toFixed(2)}%`);
     } else {
       alert("No hay rentabilidad");
     }
-  }
+  };
 }
 
-let id = 102  
+let id = 102;
 
 const aumentarId = () => {
-  id = id + 1
-}
+  id = id + 1;
+};
 
 const Items = [
-  new product (101, "Medialuna", "Pasteleria", 200, 45),
-  new product (102, "Pan de campo", "Panaderia", 500, 47)
-]
+  new product(101, "Medialuna", "Pasteleria", 200, 45),
+  new product(102, "Pan de campo", "Panaderia", 500, 47),
+];
 
-aumentarId()
-const chipa = new product (id, prompt("ingrese nombre del producto"),prompt("Ingrese categoria"), precio = parseFloat(prompt("Ingrese precio")), costo = parseFloat(prompt("Ingrese costo")))
-Items.push(chipa)
-chipa.validarRentabilidad()
+aumentarId();
+const chipa = new product(
+  id,
+  prompt("ingrese nombre del producto"),
+  prompt("Ingrese categoria"),
+  (precio = parseFloat(prompt("Ingrese precio"))),
+  (costo = parseFloat(prompt("Ingrese costo")))
+);
+Items.push(chipa);
+chipa.validarRentabilidad();
 
-aumentarId()
-const budin = new product (id, prompt("ingrese nombre del producto"),prompt("Ingrese categoria"), precio = parseFloat(prompt("Ingrese precio")), costo = parseFloat(prompt("Ingrese costo")))
-Items.push(budin)
-budin.validarRentabilidad()
+aumentarId();
+const budin = new product(
+  id,
+  prompt("ingrese nombre del producto"),
+  prompt("Ingrese categoria"),
+  (precio = parseFloat(prompt("Ingrese precio"))),
+  (costo = parseFloat(prompt("Ingrese costo")))
+);
+Items.push(budin);
+budin.validarRentabilidad();
 
-console.table(Items)
+console.table(Items);
+
+const listaProductos = document.getElementById("listaProductos");
+Items.forEach((producto) => {
+  listaProductos.innerHTML += `<tr>
+      <th>${producto.id}</th>
+      <th>${producto.nombre}</th>
+      <th>${producto.categoria}</th>
+      <th>${producto.precio}</th>
+      <th>${producto.costo}</th>
+  </tr>`;
+});
